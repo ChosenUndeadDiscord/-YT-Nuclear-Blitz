c2c7d5f157154c00b439db9bc5d44ab3e0b7320f
ChosenUndeadDiscord <36768108+ChosenUndeadDiscord@users.noreply.github.com>
2018-05-10 14:22:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
-// Set the prefix
-let prefix = "[YT]";
+let prefix = "Nuke!";
 client.on("message", (message) => {
-  // Exit and stop if the prefix is not there or if user is a bot
   if (!message.content.startsWith(prefix) || message.author.bot) return;
-
   if (message.content.startsWith(prefix + "ping")) {
     message.channel.send("pong!");
   } else
@@ -14,5 +10,8 @@ client.on("message", (message) => {
     message.channel.send("bar!");
   }
 });
-
 client.login("NDQ0MTc3NTc3NDczNDc0NTcx.DdYIKg.oKChTfc-TbP2rXUy94cck8vbuvg");
+  if (!message.content.startsWith(prefix + "account")) {
+    message.channel.send("bar!");
+  }
+});
