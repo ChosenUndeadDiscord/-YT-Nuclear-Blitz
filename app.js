4418d8cd299afac18bce00644c95040a84a656ee
ChosenUndeadDiscord <36768108+ChosenUndeadDiscord@users.noreply.github.com>
2018-05-10 12:58:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-# -YT-Nuclear-Blitz
-My Bot!
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
+// Set the prefix
+let prefix = "[YT]";
+client.on("message", (message) => {
+  // Exit and stop if the prefix is not there or if user is a bot
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (message.content.startsWith(prefix + "ping")) {
+    message.channel.send("pong!");
+  } else
+  if (message.content.startsWith(prefix + "foo")) {
+    message.channel.send("bar!");
   }
 });
 
-client.login('NDQ0MTc3NTc3NDczNDc0NTcx.DdYIKg.oKChTfc-TbP2rXUy94cck8vbuvg');
+client.login("NDQ0MTc3NTc3NDczNDc0NTcx.DdYIKg.oKChTfc-TbP2rXUy94cck8vbuvg");
